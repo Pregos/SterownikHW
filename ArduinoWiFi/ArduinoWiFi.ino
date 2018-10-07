7cc1dfd4df8e79326d8cd84791331b28cc686b02
pregos <tompreg@gmail.com>
2018-10-07 10:13:01 +0200

    Initial commit WIFI

---
+++ ArduinoWiFi/ArduinoWiFi.ino
@@ -0,0 +1,14 @@
+//Obsługa Wifi za pomocą arduino uno i esp8266
+#include <SoftwareSerial.h>
+
+//SoftwareSerial mySerial(3, 2);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
